59a8b598213f826dbea9a2ff3effb4b28a75ae04
bryant <bryantluu22@gmail.com>
2018-05-09 12:37:26 -0700

    finish problem 5

---
+++ 5.py
@@ -0,0 +1,38 @@
+"""
+
+Good morning. Here's your coding interview problem for today.
+
+This problem was asked by Jane Street.
+
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+Implement car and cdr.
+
+Upgrade to premium and get in-depth solutions to every problem.
+
+If you liked this problem, feel free to forward it along! As always, shoot us an email if there's anything we can help with!
+"""
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(pair):
+    def get_first(a, b):
+        return a
+    return pair(get_first)
+
+def cdr(pair):
+    def get_last(a, b):
+        return b
+    return pair(get_last)
+
+assert car(cons(3, 4)) == 3
+assert cdr(cons(3, 4)) == 4
