85d1647c5aca2d94c8260e25dbb5c90cde2abfd1
bryant <bryantluu22@gmail.com>
2018-05-10 05:29:00 -0700

    add description of problem 10

--- 10 - Implement a job scheduler.py
+++ 10 - Implement a job scheduler.py
@@ -1,3 +1,16 @@
+"""
+
+Good morning. Here's your coding interview problem for today.
+
+This problem was asked by Apple.
+
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+
+Upgrade to premium and get in-depth solutions to every problem.
+
+If you liked this problem, feel free to forward it along! As always, shoot us an email if there's anything we can help with!
+"""
+
 import time
 
 def job_scheduler(f, milliseconds):
